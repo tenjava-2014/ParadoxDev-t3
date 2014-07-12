646766771d0276362cfd16d8bc574bf39d77a20c
SEAN PARADOX <seanparadox.dev@gmail.com>
2014-07-12 15:12:05 +0100

    Initial project layout. It has begun. (Added main class and packages)

--- src/main/java/com/tenjava/entries/ParadoxDev/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/ParadoxDev/t3/TenJava.java
@@ -1,5 +1,25 @@
 package com.tenjava.entries.ParadoxDev.t3;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    public static Plugin plugin;
+
+    @Override
+    public void onEnable() {
+        enable();
+        System.out.println("[TenJava] ParadoxDev-t3 plugin enabled. ");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[TenJava] ParadoxDev-t3 plugin disabled.");
+    }
+
+    private void enable() {
+        plugin = this;
+    }
+
+}
